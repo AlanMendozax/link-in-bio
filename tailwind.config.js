a42345f8f23ad20d790107606a1fdb9499b5e339
AlanMendozax <alanmendoza9801@outlook.com>
2024-05-13 19:58:06 -0600

    tailwindcss configs

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    enabled: false,
+    content: ['./*.{html,js}']
+  },
+  darkMode: 'class',
+  activeMode: 'class',
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
